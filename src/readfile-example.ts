d5a95bddfe905f9dd42267df26086058be1d477f
Manuel Valles <manukempo@gmail.com>
2020-10-03 11:23:19 +0100

    Simplified read file method

--- src/readfile-example.ts
+++ src/readfile-example.ts
@@ -1,10 +1,10 @@
 import { join } from "https://deno.land/std/path/mod.ts";
 
-const readFile = async (fileName: string, folder?: string): Promise<void> => {
-    const path = folder ? join(folder, fileName) : fileName
+const readFile = async (folder: string, fileName: string): Promise<void> => {
+    const path = join(folder, fileName)
     const data: string = await Deno.readTextFile(path);
     console.log(data);
 }
 
-await readFile('hello.txt')
-await readFile('hello.txt', 'text_files')
+await readFile('.', 'hello.txt')
+await readFile('text_files', 'hello.txt')
